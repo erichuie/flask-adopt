23b53fe0abf9d57e89b718ecd0cd5a4c8ae4739c
Nathan Rydel <nathanrydel@gmail.com>
2023-12-08 17:42:13 -0700

    created routing for /add to add a pet and handle validation

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask import Flask, render_template, redirect, jsonify, flash, request
 from flask_debugtoolbar import DebugToolbarExtension
 
 
-from models import connect_db, Pet
+from models import db, connect_db, Pet
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -34,4 +35,32 @@ def show_pets():
 
 @app.route("/add", methods=["GET","POST"])
 def add_pet_form():
-    ...
+    """Add pet form with data handling"""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        from_form_name = form.name.data
+        from_form_species = form.species.data
+        from_form_photo_url = form.photo_url.data
+        from_form_age = form.age.data
+        from_form_notes = form.notes.data
+
+        new_pet = Pet(name=from_form_name,
+                species=from_form_species,
+                photo_url=from_form_photo_url,
+                age=from_form_age,
+                notes=from_form_notes)
+
+        db.session.add(new_pet)
+
+        db.session.commit()
+
+        flash(f"Pet {new_pet.name} available for adoption!")
+
+        return redirect("/")
+
+    else:
+        return render_template("/pets/add_pet_form.html")
+
+
